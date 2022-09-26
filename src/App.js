ceb72eafb92ad877d3d1456f6cfe7a332d2bc57f
Brandon Stevenson <stevenson.brandon01@gmail.com>
2022-09-26 00:09:33 -0500

    Installed react-router-dom | changed app.js to allow routing to different pages | created directories to hold content for front-end | created shopping cart and main menu page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from "./view/pages/Main";
+import Cart from "./view/pages/Cart";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main/>} /> //Routes to main page
+        <Route path="/Cart" element={<Cart/>} /> //Routes to cart page
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
