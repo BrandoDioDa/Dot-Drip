02682f251a5dd794529402f65d299ab48e630221
Brandon Stevenson <stevenson.brandon01@gmail.com>
2022-09-26 00:24:48 -0500

    Installed react-router-dom | changed app.js to allow routing to different pages | created directories to hold content for front-end | created shopping cart and main menu page | added NotFound.jsx

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Main from "./view/pages/Main";
 import Cart from "./view/pages/Cart";
+import NotFound from "./view/pages/NotFound";
+
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main/>} /> //Routes to main page
-        <Route path="/Cart" element={<Cart/>} /> //Routes to cart page
-      </Routes>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main/>} /> //Routes to main page
+                <Route path="/Cart" element={<Cart/>} /> //Routes to cart page
+                <Route path="*" element={<NotFound/>} /> //An unknown route will take you to 404
+            </Routes>
 
-    </BrowserRouter>
-  );
+        </BrowserRouter>
+    );
 }
 
 export default App;
