51a287493431b76f5254deb9b71f58e3bde69eb6
Brandon Stevenson <stevenson.brandon01@gmail.com>
2022-10-12 22:49:47 -0500

    Nav Bar is created and displays on every page | added tailwind.js (Not in use)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import Main from "./view/pages/Main";
 import Cart from "./view/pages/Cart";
 import NotFound from "./view/pages/NotFound";
+import NavBar from "./Components/navBar";
 
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
     return (
         <BrowserRouter>
+            <NavBar/>
             <Routes>
                 <Route path="/" element={<Main/>} /> //Routes to main page
                 <Route path="/Cart" element={<Cart/>} /> //Routes to cart page
                 <Route path="*" element={<NotFound/>} /> //An unknown route will take you to 404
             </Routes>
-
         </BrowserRouter>
     );
 }
