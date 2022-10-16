1dec28e28ab02b2b6c64a3f9604b0849b6eef5d5
wrinklykong <thatonejwjguy@gmail.com>
2022-10-16 14:00:25 -0500

    New page added yeah yeah

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Main from "./view/pages/Main";
 import Cart from "./view/pages/Cart";
+import Clothes from "./view/pages/Clothes";
 import NotFound from "./view/pages/NotFound";
 
 import {BrowserRouter, Routes, Route} from "react-router-dom";
@@ -10,9 +11,10 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Main/>} /> //Routes to main page
-                <Route path="/Cart" element={<Cart/>} /> //Routes to cart page
-                <Route path="*" element={<NotFound/>} /> //An unknown route will take you to 404
+                <Route path="/" element={<Main/>} />            //Routes to main page
+                <Route path="/Cart" element={<Cart/>} />        //Routes to cart page
+                <Route path="/Clothes" element={<Clothes/>} />  //Routes to the clothes page
+                <Route path="*" element={<NotFound/>} />        //An unknown route will take you to 404
             </Routes>
 
         </BrowserRouter>
