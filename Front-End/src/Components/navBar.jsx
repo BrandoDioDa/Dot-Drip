2944c765ae141e5709d5916e87fd318cd2a1ffb4
Brandon Stevenson <stevenson.brandon01@gmail.com>
2022-10-26 20:36:51 -0500

    Seperated Front-end and Back-end | installed nodemon | installed concurrently | installed mongoose |

--- Front-End/src/Components/navBar.jsx
+++ Front-End/src/Components/navBar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
-
+import Logo from "../view/images/Raindrop.png"
 const NavBar =() => {
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
             <a className="navbar-brand " href="/">
-                <img src="/src/view/images/prototype_Logo.ico" width="30" height="30" className="d-inline-block align-top" alt=""/> Dot-Drip
+                <img src={Logo} style={{width:30, height:30}} className="d-inline-block align-top" alt=""/> Dot-Drip
             </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarNavAltMarkUp" aria-controls="navbarNavAltMarkUp" aria-expanded="false"
@@ -14,14 +14,12 @@ const NavBar =() => {
                 <span className="navbar-toggler-icon"/>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkUp">
-                <div className="navbar-nav">
-                    <a className="nav-link active textColor" href={"/Clothes"}>Clothes<span className="sr-only">(current)</span></a>
-                    <a className="nav-link active textColor" href="#">Shoes<span className="sr-only">(current)</span></a>
-                    <a className="nav-link active textColor" href="#">Popular-Items<span className="sr-only">(current)</span></a>
-                </div>
+                <a className="nav-link active textColor" href={"/Clothes"}>Clothes<span className="sr-only">(current)</span></a>
+                <a className="nav-link active textColor" href="#">Shoes<span className="sr-only">(current)</span></a>
+                <a className="nav-link active textColor" href="#">Popular-Items<span className="sr-only">(current)</span></a>
                 <form className="d-flex">
                     <Link to="/Signin"><button className="btn btn-outline-secondary my-2 my-sm-0"  type="button" placeholder="Login">Sign-up</button></Link>
-                    <Link to="/Login"><button className="btn btn-outline-success my-2 my-sm-0" type="button" placeholder="Login">Log-in</button></Link>
+                    <Link to={"/Login"}><button className="btn btn-outline-success my-2 my-sm-0" type="button" placeholder="Login">Log-in</button></Link>
                 </form>
             </div>
             </div>
