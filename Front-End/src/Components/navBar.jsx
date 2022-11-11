a14ac0d3c0b921a8fdefff486b0187acd744e5f1
Brandon Stevenson <stevenson.brandon01@gmail.com>
2022-11-11 14:01:21 -0600

    fixed navbar warning

--- Front-End/src/Components/navBar.jsx
+++ Front-End/src/Components/navBar.jsx
@@ -15,8 +15,8 @@ const NavBar =() => {
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkUp">
                 <a className="nav-link active textColor" href={"/Clothes"}>Clothes<span className="sr-only">(current)</span></a>
-                <a className="nav-link active textColor" href="#">Shoes<span className="sr-only">(current)</span></a>
-                <a className="nav-link active textColor" href="#">Popular-Items<span className="sr-only">(current)</span></a>
+                <a className="nav-link active textColor" href="/">Shoes<span className="sr-only">(current)</span></a>
+                <a className="nav-link active textColor" href="/">Popular-Items<span className="sr-only">(current)</span></a>
                 <form className="d-flex">
                     <Link to="/Signin"><button className="btn btn-outline-secondary my-2 my-sm-0"  type="button" placeholder="Login">Sign-up</button></Link>
                     <Link to={"/Login"}><button className="btn btn-outline-success my-2 my-sm-0" type="button" placeholder="Login">Log-in</button></Link>
