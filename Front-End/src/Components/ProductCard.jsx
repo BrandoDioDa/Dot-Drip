63fb7912f37ba75dadb7f872a2fe75e42b176d57
wrinklykong <thatonejwjguy@gmail.com>
2022-11-18 14:14:00 -0600

    Handles image loading errors

--- Front-End/src/Components/ProductCard.jsx
+++ Front-End/src/Components/ProductCard.jsx
@@ -3,20 +3,17 @@ import notFound from "../view/images/notfound.jpg";
 
 
 const ProductCard = (props) => {
-    var productImage = props.product.prodImage;
-    if (productImage === ""){
-        productImage = notFound;
-    }
-    // tHis does not check for a HTTP 404, but when there isnt an image specified it will default to this, need to fix
+
     return (
         <div>
             <div className='ProductCard'>
                 <b>{ props.product.prodName }</b>
                     <br></br>
                     <img 
-                        src={productImage} 
+                        src={props.product.prodImage} 
                         alt={props.product.prodName} 
                         class="productImage"
+                        onError={(e) => { e.target.onError = null; e.target.src = notFound}}
                     ></img>
                 <br></br>
                 { props.product.prodDesc }
