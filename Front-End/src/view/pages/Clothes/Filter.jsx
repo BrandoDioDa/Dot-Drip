d857b82730681d8ca6c41cb0ab77f7c249c014bf
its_ric0 <ricodav.98@gmail.com>
2022-11-29 14:23:04 -0600

    added search bar and go to cart button to filter

--- Front-End/src/view/pages/Clothes/Filter.jsx
+++ Front-End/src/view/pages/Clothes/Filter.jsx
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
 import Button from "react-bootstrap/esm/Button";
+import Form from 'react-bootstrap/Form';
 import "../../cssDesign/clothes.css";
 import "../../cssDesign/filter.css";
 
 const Filter =() => {
     return (
-            <Dropdown align="end" autoClose="inside">
+            <Dropdown size="normal" align="end" autoClose="inside">
              
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    Filter
-                </Dropdown.Toggle>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">Filter</Dropdown.Toggle>
                 <Dropdown.Menu>
+                    <Form>
+                        <Form.Control size="normal" type="search" placeholder="Search for a product..."></Form.Control>
+                    </Form>
+
+                <Dropdown.Divider/>
                     <Dropdown.Item href="#/action-1">Men</Dropdown.Item>
                     <Dropdown.Item href="#/action-2">Women</Dropdown.Item>
                     <Dropdown.Item href="#/action-3">Kids</Dropdown.Item>
+                        
+                <Dropdown.Divider/>
+
+                   <Button href="#/action-4" variant="outline-info" size="normal">Go to Cart</Button>
+
                 </Dropdown.Menu>
             </Dropdown>
+            
     );
 }
 
