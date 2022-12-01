862fca7e4e5b88e51015d5fcc35c6add72c1143b
wrinklykong <thatonejwjguy@gmail.com>
2022-12-01 01:41:46 -0600

    Initial commit help me plz

--- Front-End/src/view/pages/Clothes/Clothes.jsx
+++ Front-End/src/view/pages/Clothes/Clothes.jsx
@@ -3,16 +3,137 @@ import "../../cssDesign/clothes.css"
 import ClothesHeader from "./Header";
 import ProductDisplay from "./ProductDisplay";
 import Filter from "./Filter";
+import React, { useState, useEffect } from "react";
+import {getAllProducts, getProductById, getProductsFromQuery} from "../../../services/productsService";
 
-const Clothes =() => {
+export default function Clothes(){
+
+    const [products, setProducts] = useState([]);
+    const [allProducts, setAllProducts] = useState([]);
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    async function getProducts() {
+        const response = await getAllProducts();
+        setProducts(response.data);
+        setAllProducts(response.data);
+    }
+
+    async function showShirts() {
+        console.log("Showing shirts");
+        try {
+            const response = await getProductsFromQuery(JSON.stringify({prodType: "shirt"}));
+            setProducts(response.data);
+        }
+        catch {
+            console.log("error happened");
+        }
+    }
+
+    async function showShoes() {
+        try {
+            const response = await getProductsFromQuery(JSON.stringify({prodType: "shoe"}));
+            setProducts(response.data);
+        }
+        catch {
+            console.log("error happened");
+        }
+    }
+
+    async function sortByQuantity(type) {
+        if ( type === "ASCENDING" ) {
+            const sorted = products.sort((a, b) => (a.prodQuan > b.prodQuan) ? 1 : -1);
+            setProducts( sorted.map((item) => item) );
+        }
+        else if ( type === "DESCENDING" ) {
+            const sorted = products.sort((a, b) => (a.prodQuan > b.prodQuan) ? -1 : 1);
+            setProducts( sorted.map((item) => item) );
+        }
+        else {
+            console.log("Unknown type on sortByQuantity. Please check your call!");
+        }
+    }
+
+    async function sortByPrice(type) {
+        if ( type === "ASCENDING" ) {
+            const sorted = products.sort((a, b) => (Number(a.prodPrice.replace(/[^0-9.-]+/g,"")) > Number(b.prodPrice.replace(/[^0-9.-]+/g,""))) ? 1 : -1);
+            setProducts( sorted.map((item) => item) );
+        }
+        else if ( type === "DESCENDING" ) {
+            const sorted = products.sort((a, b) => (Number(a.prodPrice.replace(/[^0-9.-]+/g,"")) > Number(b.prodPrice.replace(/[^0-9.-]+/g,""))) ? -1 : 1);
+            setProducts( sorted.map((item) => item) );
+        }
+        else {
+            console.log("Unknown type on sortByPrice. Please check your call!");
+        }
+    }
+
+    async function unshowItem() {
+        const response = await getAllProducts();
+        setProducts( response.data );
+    }
+
+    function unCheck() {
+        var x = document.getElementsByClassName("btn-check");
+        console.log(x);
+        for(var i=0; i<=x.length; i++) {
+            if ( x[i].checked != null ) { 
+                x[i].checked = false;
+            }
+        }   
+        unshowItem();
+      }
+
+    const updateCheck = (e) => {
+        const id = e.target.id;
+        if (e.target.checked) {
+            if ( id == "shirt-check" ) {
+                showShirts().then();
+            }
+            else if ( id == "shoes-check" ) {
+                showShoes().then();
+            }
+            else if ( id === "quantity-sort-asc" ) {
+                sortByQuantity("ASCENDING").then();
+            }
+            else if ( id === "quantity-sort-des" ) {
+                sortByQuantity("DESCENDING").then();
+            }
+            else if ( id === "price-sort-asc" ) {
+                sortByPrice("ASCENDING");
+            }
+            else if ( id === "price-sort-des" ) {
+                sortByPrice("DESCENDING");
+            }
+            else {
+                console.log("Error: ID Not found!");
+            }
+        }
+        else {
+            unshowItem();
+        }
+    }
+
     return(
         <div className="BackgroundColor">
             <ClothesHeader/>
-            <Filter/>
-            <ProductDisplay/>
+            <Filter 
+                showShirts = {showShirts}
+                showShoes = {showShoes}
+                sortByQuantity ={(type) => {
+                    sortByQuantity(type);
+                }}
+                sortByPrice ={(type) => {
+                    sortByPrice(type);
+                }}
+                unshowItem = {unshowItem}
+            />
+            <ProductDisplay products={products}/>
         </div>
     );
 }
 
-export default Clothes;
+//export default Clothes;
 
--- Front-End/src/view/pages/Clothes/Filter.jsx
+++ Front-End/src/view/pages/Clothes/Filter.jsx
@@ -1,20 +1,50 @@
 import React, { useState, useEffect } from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
-import "../../cssDesign/clothes.css"
+import Button from "react-bootstrap/esm/Button";
+import Form from 'react-bootstrap/Form';
+import showShirts from './ProductDisplay';
+import {setAllProducts, getAllProducts, setProducts} from './ProductDisplay';
 
-const Filter =() => {
+import "../../cssDesign/clothes.css";
+
+export default function Filter (props) {
+
+    const handleFunction = (e) => {
+        console.log("Show shirts");
+    }
+
     return (
-            <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    Dropdown Button
-                </Dropdown.Toggle>
+        <div className="container">
+            <Dropdown size="normal" align="end" autoClose="inside">
+                <Dropdown.Toggle variant="success" id="dropdown-basic">Filter</Dropdown.Toggle>
                 <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    <Form>
+                        <Form.Control size="normal" type="search" placeholder="Search for a product..."></Form.Control>
+                    </Form>
+
+                <hr className="hr hr-blurry"/>
+                    <Dropdown.Item id="men-search" onClick={props.func}>Men</Dropdown.Item>
+                    <Dropdown.Item id="women-search">Women</Dropdown.Item>
+                    <Dropdown.Item id="kids-search">Kids</Dropdown.Item>
+                <Dropdown.Divider/>
+                    <Dropdown.Header>Sort by:</Dropdown.Header>
+                    <Dropdown.Item id="price-inc" onClick={props.showShirts}>Price(inc)</Dropdown.Item>
+                    <Dropdown.Item id="price-dec" onClick={props.showShoes}>Price(dec)</Dropdown.Item>
+                    <Dropdown.Item id="quan-inc" onClick={handleFunction}>Quantity(inc)</Dropdown.Item>
+                    <Dropdown.Item id="quan-dec" onClick={handleFunction}>Quantity(dec)</Dropdown.Item>
+                <Dropdown.Divider/>
+                    <Dropdown.Header>Item Type:</Dropdown.Header>
+                    <Dropdown.Item id="shirt-search" onClick={props.showShirts}>Shirt</Dropdown.Item>
+                    <Dropdown.Item id="pants-search" onClick={props.showPants}>Shoes</Dropdown.Item>
+                    <Dropdown.Item id="unimplemented" onClick={handleFunction}>Other</Dropdown.Item>
+                <Dropdown.Divider/>
+
+                   <Button href="#/action-11" variant="outline-info" size="normal">Go to Cart</Button>
+
                 </Dropdown.Menu>
             </Dropdown>
+        </div>
     );
 }
 
-export default Filter;
+//export default Filter;
